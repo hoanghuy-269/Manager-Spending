b8a47a4a6d33252d5f4b964fec6a142806de15ec
hoanghuy-269 <hoanghuy29625@gmail.com>
2025-11-01 08:24:13 +0300

    set up

---
+++ Spending-Manager/Spending-Manager/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spending-Manager
+//
+//  Created by  User on 30.10.2025.
+//
+    
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
