0852deb11882a0d76b81ddbe8d521859bf9aaee0
thong <tommai2122004-pixel>
2025-11-12 10:50:03 +0300

    xong luong thong ke

---
+++ Spending-Manager/Spending-Manager/Cells/TransactionCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionCellTableViewCell.swift
+//  Spending-Manager
+//
+//  Created by  User on 09.11.2025.
+//
+
+import UIKit
+
+class TransactionCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+}
---
+++ Spending-Manager/Spending-Manager/Controller/columnStatisticsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  columnStatisticsViewController.swift
+//  Spending-Manager
+//
+//  Created by  User on 11.11.2025.
+//
+
+import UIKit
+
+class columnStatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
